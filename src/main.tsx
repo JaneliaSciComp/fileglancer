8eb6715f82f3b6bf48ca90d724e38fd00d04d6b0
allison-truhlar <truhlara@janelia.hhmi.org>
2025-10-06 16:49:04 -0400

    chore: set up  queryClient, QueryClientProvider and devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import './index.css';
 import App from './App';
 
+const queryClient = new QueryClient();
+
+// From Tanstack Query DevTools extension instructions
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient;
+  }
+}
+window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
