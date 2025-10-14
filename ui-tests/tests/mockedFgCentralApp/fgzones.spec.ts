772d2f564e9f47d455caecbf8e85284bd9099a9b
allison-truhlar <truhlara@janelia.hhmi.org>
2025-10-14 14:20:03 -0400

    refactor: change how page is reloaded; remove unneeded selectors

--- ui-tests/tests/mockedFgCentralApp/fgzones.spec.ts
+++ ui-tests/tests/mockedFgCentralApp/fgzones.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@jupyterlab/galata';
+import { expect, test } from '@playwright/test';
 import {
   openFileGlancer,
   mockAPI,
@@ -47,23 +47,10 @@ test('favor entire zone with reload page', async ({ page }) => {
   await expect(page.getByText('May 21, 2025')).toBeVisible();
   await expect(page.getByText('10 bytes').first()).toBeVisible();
 
-  const z2ExpandedStarButton = page
-    .getByRole('list')
-    .filter({ hasText: 'Z1homeprimaryZ2scratch' })
-    .getByRole('button')
-    .nth(3);
-
-  await expect(
-    z2ExpandedStarButton.locator('svg path[fill-rule]') // filled star
-  ).toHaveCount(0);
-  await expect(
-    z2ExpandedStarButton.locator('svg path[stroke-linecap]') // empty star
-  ).toHaveCount(1);
-
   // favor entire Z2
   await page
     .getByRole('listitem')
-    .filter({ hasText: 'Z2' })
+    .filter({ hasText: 'Z2', hasNotText: 'scratch' })
     .getByRole('button')
     .click();
 
@@ -74,10 +61,9 @@ test('favor entire zone with reload page', async ({ page }) => {
   // test that Z2 now shows in the favorites
   await expect(Z2favorite).toBeVisible();
 
-  // reload page - somehow page.reload hangs so I am going back to jupyterlab page
-  await openFileGlancer(page);
+  // reload page to verify favorites persist
+  await page.reload();
 
-  const z2CollapsedStarButton = page.getByRole('button').nth(4);
   // test Z2 still shows as favorite
   await expect(Z2favorite).toBeVisible();
 });
