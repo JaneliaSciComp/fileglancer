ec357fdeef486b9d690e70436ea612a2350bf726
allison-truhlar <truhlara@janelia.hhmi.org>
2025-10-30 12:01:02 -0400

    fix: isolate db per test by having tests run in sequence, not parallel

--- frontend/ui-tests/playwright.config.js
+++ frontend/ui-tests/playwright.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
   },
   timeout: process.env.CI ? 90_000 : 10_000,
   navigationTimeout: process.env.CI ? 90_000 : 10_000,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   webServer: {
     command: 'pixi run test-launch',
     url: 'http://localhost:7879/fg/',
