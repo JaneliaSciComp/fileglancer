9faf085be350c97ceff3dcd77906234f29ae5ace
Allison Truhlar <truhlara@janelia.hhmi.org>
2026-04-07 13:22:23 -0400

    feat: add transparent data links option; renamed component to reflect multiple opts

--- frontend/src/components/ui/PreferencesPage/DataLinkOptions.tsx
+++ frontend/src/components/ui/PreferencesPage/DataLinkOptions.tsx
@@ -3,39 +3,61 @@ import toast from 'react-hot-toast';
 import { usePreferencesContext } from '@/contexts/PreferencesContext';
 import OptionsSection from '@/components/ui/PreferencesPage/OptionsSection';
 
-type AutomaticLinksToggleProps = {
+type DataLinkOptionsProps = {
   readonly checkboxesOnly?: boolean;
 };
 
-export default function AutomaticLinksToggle({
+export default function DataLinkOptions({
   checkboxesOnly = false
-}: AutomaticLinksToggleProps) {
-  const { areDataLinksAutomatic, toggleAutomaticDataLinks } =
-    usePreferencesContext();
+}: DataLinkOptionsProps) {
+  const {
+    areDataLinksAutomatic,
+    toggleAutomaticDataLinks,
+    transparentDataLinks,
+    toggleTransparentDataLinks
+  } = usePreferencesContext();
+
+  const automaticOption = {
+    checked: areDataLinksAutomatic,
+    id: 'automatic_data_links',
+    label: 'Enable automatic data link creation',
+    onChange: async () => {
+      const result = await toggleAutomaticDataLinks();
+      if (result.success) {
+        toast.success(
+          areDataLinksAutomatic
+            ? 'Disabled automatic data links'
+            : 'Enabled automatic data links'
+        );
+      } else {
+        toast.error(result.error);
+      }
+    }
+  };
+
+  const transparentOption = {
+    checked: transparentDataLinks,
+    id: 'transparent_data_links',
+    label: 'Include full path in data links',
+    onChange: async () => {
+      const result = await toggleTransparentDataLinks();
+      if (result.success) {
+        toast.success(
+          transparentDataLinks
+            ? 'Disabled full path in data links'
+            : 'Enabled full path in data links'
+        );
+      } else {
+        toast.error(result.error);
+      }
+    }
+  };
 
   return (
     <OptionsSection
       checkboxesOnly={checkboxesOnly}
       header="Data Links"
-      options={[
-        {
-          checked: areDataLinksAutomatic,
-          id: 'automatic_data_links',
-          label: 'Enable automatic data link creation',
-          onChange: async () => {
-            const result = await toggleAutomaticDataLinks();
-            if (result.success) {
-              toast.success(
-                areDataLinksAutomatic
-                  ? 'Disabled automatic data links'
-                  : 'Enabled automatic data links'
-              );
-            } else {
-              toast.error(result.error);
-            }
-          }
-        }
-      ]}
+      options={[automaticOption, transparentOption]}
     />
   );
 }
